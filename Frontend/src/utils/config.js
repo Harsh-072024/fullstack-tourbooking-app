65c8679cc2a4e68f514b43eb3b398cc90ae8295e
raushan <rk0383944@gmail.com>
2025-01-30 12:59:30 +0530

    project completed now

--- Frontend/src/utils/config.js
+++ Frontend/src/utils/config.js
@@ -1,3 +1,5 @@
-export const BASE_URL = 'http://localhost:4000/api/v1'
-// export const BASE_URL = 'https://tour-full-stack-rosy.vercel.app/api/v1'
-// export const BASE_URL = 'https://tour-full-stack-rosy.vercel.app';
+export const BASE_URL = import.meta.env.MODE === "development" 
+  ? "http://localhost:4000/api/v1" 
+  : "/api/v1";
+
+
